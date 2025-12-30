524ec1603020cf10b0b3e37277d55d19b9961e9b
ila saxena <ilasaxena@ilas-MacBook-Air.local>
2025-12-30 12:34:02 +0530

    created weekday component

---
+++ src/components/Calendar/Weekdays.tsx
@@ -0,0 +1,14 @@
+import { WEEKDAYNAME_LIST } from '../../constants';
+import DateCell from './DateCell';
+
+const Weekdays: React.FC = () => {
+  return (
+    <div className="p-2 m-4 grid grid-cols-7">
+      {WEEKDAYNAME_LIST.map((weekDay) => {
+        return <DateCell day={weekDay} isHighlighted={false} />;
+      })}
+    </div>
+  );
+};
+
+export default Weekdays;
