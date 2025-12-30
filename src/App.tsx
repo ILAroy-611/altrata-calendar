3c6acbd0d4a3cfe2769866a056032771f147a696
ila saxena <ilasaxena@ilas-MacBook-Air.local>
2025-12-30 11:07:50 +0530

    eslint & prettier setup done, react 18 installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
 import './App.css';
 
 function App() {
   return (
-    <div className='py-8 px-16 w-1/2 m-auto text-center'>
-      Calender App
+    <div className="py-8 px-16 w-1/2 m-auto text-center">
+      <p>Calender App</p>
     </div>
   );
 }
