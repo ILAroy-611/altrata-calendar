58c0d2cec063445541447b0c610a97da6175b667
ila saxena <ilasaxena@ilas-MacBook-Air.local>
2025-12-30 14:13:19 +0530

    added utilities to extract total days in month, starting week day & to generate calendar data

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,3 +9,31 @@ export const extractYear = (date: Date): number => {
   const year = date.getFullYear();
   return year;
 };
+
+export const extractStartingWeekdayOfMonth = (date: Date): number => {
+  const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+  const weekdayIndex = firstDayOfMonth.getDay();
+  return weekdayIndex;
+};
+
+export const getTotalDaysInMonth = (date: Date) => {
+  const totalDays = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+  return totalDays;
+};
+
+export const generateCalendarRows = (date: Date) => {
+  const startingWeekdayIndex = extractStartingWeekdayOfMonth(date);
+  const totalDays = getTotalDaysInMonth(date);
+  let startingDate = 1;
+  let result = [];
+  for (let i = 0; i <= startingWeekdayIndex; i++) {
+    if (i === startingWeekdayIndex) {
+      for (let j = startingDate; j <= totalDays; j++) {
+        result.push(j);
+      }
+    } else {
+      result.push(null);
+    }
+  }
+  return result;
+};
